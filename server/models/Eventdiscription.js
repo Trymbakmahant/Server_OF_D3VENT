77d176d7ca0bc0a8ee558cb7626f97b700098fa5
Vaibhav Arora <aroravaibhav817@gmail.com>
2022-09-22 18:40:18 +0530

    made some changes

--- server/models/Eventdiscription.js
+++ server/models/Eventdiscription.js
@@ -2,12 +2,10 @@ const mongoose = require("mongoose");
 
 const eventDiscription = new mongoose.Schema({
   indexId: { type: String, required: true },
-  Time: { type: String, required: true },
   eventName: { type: String, required: true },
-  eventUrl: { type: String, required: true },
   AddvertiseLink: { type: String, required: true },
   AddvertiseName: { type: String, required: true },
-  CurrentNo: { type: String, required: true },
+  timeToEnd: {type: Number, required: true}
 });
 
 const EventDiscription = mongoose.model("EventDiscription", eventDiscription);
